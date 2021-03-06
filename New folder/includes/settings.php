f73460095514819f8a5a6d603ff79b74f18de0ca
mohamad <79378275+mohamad9898401@users.noreply.github.com>
2021-03-06 06:28:15 +0330

    فایل های جدید پروژه از مزرعه تا فنجان قهوه
    به دلیل قطعی شدیدی اینترنت برای آپلود به مشکل خوردم

---
+++ New folder/includes/settings.php
@@ -0,0 +1,19 @@
+<?php
+if( ! defined('DBHOST') )
+	define( 'DBHOST', 'sql200.b6b.ir' );
+if( ! defined('DBUSER') )
+	define( 'DBUSER', 'b6bi_27936531' );
+if( ! defined('DBPASS') )
+	define( 'DBPASS', '' );
+if( ! defined('DBNAME') )
+	define( 'DBNAME', 'b6bi_27936531_w449' );
+	
+if( ! defined('CHARSET') )
+	define( 'CHARSET', 'utf8mb4_unicode_520_ci' );
+
+$dbHost =	DBHOST;
+$dbUser =	DBUSER;
+$dbPass =	DBPASS;
+$dbname =	DBNAME;
+
+$charset =	CHARSET;
