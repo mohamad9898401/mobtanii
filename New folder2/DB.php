84e5e948ea7cc0ac1e6dbcf3b1c0e04f2625c1f8
mohamad <79378275+mohamad9898401@users.noreply.github.com>
2021-03-06 07:05:47 +0330

    Add files via upload

---
+++ New folder2/DB.php
@@ -0,0 +1,47 @@
+﻿<?php
+if(! class_exists('DB')) {
+	class DB{
+		var $dbc;
+		function __construct(){
+			$this -> connect();
+			$this -> selectDB();
+			$this -> dbc -> set_charset( CHARSET );
+		}
+		
+		function connect(){
+			$this -> dbc = new mysqli(sql200.b6b.ir, b6bi_27936531,,b6bi_27936531_w449); 
+			if( $this -> dbc -> connect_error ){
+				$error = "
+						خطا در اتصال به دیتابیس!
+						<section lang = 'en'>{$this -> dbc -> connect_error}</section>";
+				die( $error );
+			}
+		}
+		function selectDB(){
+			$this -> dbc -> select_db(DBNAME);
+			if( $this -> dbc -> error ){
+				$error = "
+						خطا در انتخاب دیتابیس!
+						<section lang = 'en'>{$this -> dbc -> error}</section>";
+				die( $error );
+			}
+		}
+
+		function execute( $sql ){
+			$result = $this -> dbc -> query( $sql ); // mysqli_query
+			if( $this -> dbc -> error ){
+				$error = "
+						خطا در اجرای کوئری!
+						<section lang = 'en'>{$sql}<br>
+						{$this -> dbc -> error}</section>";
+				die( $error );
+			}
+			else
+				return true;
+		}
+		
+		function __destruct(){
+			$this -> dbc -> close(); //mysqli_close
+		}
+	}
+}
